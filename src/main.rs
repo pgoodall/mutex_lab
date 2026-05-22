614d9f54cfefd55c3f2af6b241d6c81189f32612
Pete Goodall <pete@digicratic.com>
2026-05-22 23:16:59 +0100

    Working version

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,21 @@ use std::sync::Mutex;
 fn main() {
     let mut data = Mutex::new(vec![1, 2, 3]);
 
-    for i in 0..3 {
+    // for i in 0..3 {
         // Try to capture a mutable reference in multiple threads
         // This will fail to compile!
-        let handle = thread::spawn(move || {
+    let handle = thread::spawn(move || {
+        for i in 0..3 {
             let mut value = data.lock().unwrap();
             value[i] += 1;
-        });
+        }
+        data
+    });
 
-        handle.join().unwrap();
-
+    match handle.join() {
+        Ok(result) => println!("Thread finished with result: {:?}", result),
+        Err(e) => println!("Thread panicked: {:?}", e),
     }
+
+    //println!("Data: {:?}", data.lock().unwrap());
 }
